50040e43cfe7c596340b17c8d8ee97609ac65255
vishalmbk! <vishalbkasbe@gmail.com>
2022-01-06 11:36:33 +0530

    Restro Frist

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Restrurent from './Components/Basics/Restrurent'
+
+const App = () => {
+  return (
+    <div>
+      
+      <Restrurent/>
+    </div>
+  )
+}
+
+export default App 
